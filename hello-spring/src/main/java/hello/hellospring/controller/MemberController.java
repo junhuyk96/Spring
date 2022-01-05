fd43a7f8b18c97a785c83201bb2f75ddcfce495a
junhuyk96 <junhuyk96@gmail.com>
2022-01-05 23:41:10 +0900

    'commit'

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -25,7 +25,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/membeers/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
