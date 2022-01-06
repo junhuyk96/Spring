5954e0b132d38d1818bad4271a09e4cf17bb3a27
junhuyk96 <junhuyk96@gmail.com>
2022-01-06 20:36:01 +0900

    'commit'

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -26,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
         return new JdbcMemberRepository(dataSource);
     }
 }
