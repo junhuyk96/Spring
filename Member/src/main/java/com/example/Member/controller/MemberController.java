0ba6daa148f3d1b2e8467c272e291e97d3be69eb
junhuyk96 <junhuyk96@gmail.com>
2022-01-20 23:48:02 +0900

    'commit'

--- Member/src/main/java/com/example/Member/controller/MemberController.java
+++ Member/src/main/java/com/example/Member/controller/MemberController.java
@@ -1,13 +1,13 @@
 package com.example.Member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-    @GetMapping("/spring")
-    public String sample() {
-        return "No Way";
+    @GetMapping("input")
+    public String input_name(@RequestParam("name") String name) {
+        return "name : " + name;
     }
-
 }
