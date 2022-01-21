376337c678d7e3514b689788b4dba29b4bacdc5d
junhuyk96 <junhuyk96@gmail.com>
2022-01-21 22:58:13 +0900

    'commit'

---
+++ Member/src/main/java/com/example/Member/Repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.example.Member.Repository;
+
+import com.example.Member.domain.Member;
+
+public interface MemberRepository {
+     Member save(Member member);
+}
--- Member/src/main/java/com/example/Member/controller/MemberController.java
+++ Member/src/main/java/com/example/Member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.Member.controller;
 
+import com.example.Member.domain.Member;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
     @GetMapping("input")
-    public String input_name(@RequestParam("name") String name) {
-        return "name : " + name;
+    public String input_name(@RequestParam("name") String name,
+        @RequestParam("id") String id) {
+        Member member = new Member();
+
+        member.setId(id);
+        member.setName(name);
+
+        return "ID : " + member.getId() + " NAME : " + member.getName();
     }
 }
