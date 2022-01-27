03e91e356ee3f8ec43bb4d085ba2c3db596b88b5
junhuyk96 <junhuyk96@gmail.com>
2022-01-27 11:34:58 +0900

    'commit'

---
+++ Book/src/main/java/com/example/Book/BookController.java
@@ -0,0 +1,14 @@
+package com.example.Book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BookController {
+    @RequestMapping(value="/create", method = RequestMethod.GET)
+    public ModelAndView create() {
+        return new ModelAndView("create.html");
+    }
+}
